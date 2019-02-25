fdf7c7eeeb2979187f1229cd93b77a8ba022c602
zakjakub <mail@jakubzak.eu>
2019-02-25 13:06:46 +0100

    Some changes.

--- Resources/config/bundles.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\MonologBundle\MonologBundle::class                 => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-];
