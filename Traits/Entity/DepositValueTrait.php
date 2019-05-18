6158a6bd1fe068b1379d455f86ea03b8e1d24769
zakjakub <mail@jakubzak.eu>
2019-05-18 08:07:32 +0200

    Added DepositValueTrait.

---
+++ Traits/Entity/DepositValueTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Zakjakub\OswisCoreBundle\Traits\Entity;
+
+trait DepositValueTrait
+{
+
+    /**
+     * Numeric value of deposit.
+     *
+     * @var int|null
+     *
+     * @Doctrine\ORM\Mapping\Column(type="integer", nullable=false, options={"default": 0})
+     */
+    private $depositValue;
+
+    /**
+     * Get deposit value.
+     *
+     * @return int
+     */
+    final public function getDepositValue(): int
+    {
+        return $this->depositValue ?? 0;
+    }
+
+    /**
+     * Set deposit value.
+     *
+     * @param int $depositValue
+     */
+    final public function setDepositValue(int $depositValue): void
+    {
+        $this->depositValue = $depositValue ?? 0;
+    }
+}
