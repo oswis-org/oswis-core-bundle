5c6f83f388dc8e77c2ffa6b27e8eab9e9735b4a6
zakjakub <mail@jakubzak.eu>
2019-05-26 10:35:20 +0200

    Some changes.

---
+++ Traits/Entity/GeoCoordinatesTrait.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Zakjakub\OswisCoreBundle\Traits\Entity;
+
+trait GeoCoordinatesTrait
+{
+
+    /**
+     * @var int|null
+     * @Doctrine\ORM\Mapping\Column(type="integer", nullable=true)
+     */
+    protected $geoCoordinatesLength;
+
+    /**
+     * Geo latitude (WGS 84).
+     * @var int|null
+     * @Doctrine\ORM\Mapping\Column(type="integer", nullable=true)
+     * @example 37.42242
+     */
+    protected $geoLatitude;
+
+    /**
+     * Geo longitude (WGS 84).
+     * @var int|null
+     * @Doctrine\ORM\Mapping\Column(type="integer", nullable=true)
+     * @example -122.08585
+     */
+    protected $geoLongitude;
+
+    /**
+     * Geo elevation (WGS 84, in meters).
+     * @var int|null
+     * @Doctrine\ORM\Mapping\Column(type="integer", nullable=true)
+     * @example 1000
+     */
+    protected $geoElevation;
+
+    /**
+     * @return int
+     */
+    final public function getGeoLongitude(): ?int
+    {
+        return $this->geoLongitude;
+    }
+
+    /**
+     * @param int $geoLongitude
+     */
+    final public function setGeoLongitude(?int $geoLongitude): void
+    {
+        $this->geoLongitude = $geoLongitude;
+    }
+
+    /**
+     * @return int
+     */
+    final public function getGeoLatitude(): ?int
+    {
+        return $this->geoLatitude;
+    }
+
+    /**
+     * @param int $geoLatitude
+     */
+    final public function setGeoLatitude(?int $geoLatitude): void
+    {
+        $this->geoLatitude = $geoLatitude;
+    }
+
+
+    /**
+     * @return int
+     */
+    final public function getGeoElevation(): ?int
+    {
+        return $this->geoElevation;
+    }
+
+    /**
+     * @param int $geoElevation
+     */
+    final public function setGeoElevation(?int $geoElevation): void
+    {
+        $this->geoElevation = $geoElevation;
+    }
+
+}
