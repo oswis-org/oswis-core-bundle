7c3415583476f5f54468e3c7c3b08b55196a08f6
zakjakub <mail@jakubzak.eu>
2019-06-16 17:13:37 +0200

    Some changes.

---
+++ Traits/Entity/EntityDeletedContainerTrait.php
@@ -0,0 +1,30 @@
+<?php
+/** @noinspection PhpUndefinedMethodInspection */
+
+namespace Zakjakub\OswisCoreBundle\Traits\Entity;
+
+use DateTime;
+
+trait EntityDeletedContainerTrait
+{
+
+    final public function setDeleted(?DateTime $deletedDateTime): void
+    {
+        if ($this->getDeleted() !== $deletedDateTime) {
+            $newRevision = clone $this->getRevision();
+            $newRevision->setDeleted($deletedDateTime);
+            $this->addRevision($newRevision);
+        }
+    }
+
+    final public function getDeleted(?DateTime $referenceDateTime = null): bool
+    {
+        return $this->getRevisionByDate($referenceDateTime)->getDeleted();
+    }
+
+    final public function isDeleted(?DateTime $referenceDateTime = null): bool
+    {
+        return (bool)$this->getRevisionByDate($referenceDateTime)->getDeleted();
+    }
+
+}
