6b6a717221564f6e93b4043993ef9835f426b727
zakjakub <mail@jakubzak.eu>
2019-07-22 19:56:27 +0200

    Some changes.

---
+++ Traits/Entity/TypeContainerTrait.php
@@ -0,0 +1,29 @@
+<?php
+/** @noinspection PhpUndefinedMethodInspection */
+
+namespace Zakjakub\OswisCoreBundle\Traits\Entity;
+
+use DateTime;
+
+trait TypeContainerTrait
+{
+
+    /**
+     * @param string|null $type
+     *
+     * @throws RevisionMissingException
+     */
+    final public function setType(?string $type): void
+    {
+        if ($this->getType() !== $type) {
+            $newRevision = clone $this->getRevision();
+            $newRevision->setType($type);
+            $this->addRevision($newRevision);
+        }
+    }
+
+    final public function getType(?DateTime $dateTime = null): ?string
+    {
+        return $this->getRevisionByDate($dateTime)->getType();
+    }
+}
