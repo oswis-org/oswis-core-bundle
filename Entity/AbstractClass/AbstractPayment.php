5328c2c16079f04d87cb20193d58d055dfad014c
zakjakub <mail@jakubzak.eu>
2019-07-22 10:26:13 +0200

    Some changes.

---
+++ Entity/AbstractClass/AbstractPayment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Zakjakub\OswisCoreBundle\Entity\AbstractClass;
+
+use Zakjakub\OswisCoreBundle\Traits\Entity\BasicEntityTrait;
+use Zakjakub\OswisCoreBundle\Traits\Entity\BasicMailConfirmationTrait;
+use Zakjakub\OswisCoreBundle\Traits\Entity\DateTimeTrait;
+use Zakjakub\OswisCoreBundle\Traits\Entity\InternalNoteTrait;
+use Zakjakub\OswisCoreBundle\Traits\Entity\NoteTrait;
+use Zakjakub\OswisCoreBundle\Traits\Entity\NumericValueTrait;
+use Zakjakub\OswisCoreBundle\Traits\Entity\TypeTrait;
+
+abstract class AbstractPayment
+{
+
+    use BasicEntityTrait;
+    use DateTimeTrait;
+    use NumericValueTrait;
+    use TypeTrait;
+    use NoteTrait;
+    use InternalNoteTrait;
+    use BasicMailConfirmationTrait;
+
+    public static function getAllowedTypesDefault(): array
+    {
+        return [
+            '',
+            'administration',
+        ];
+    }
+
+    public static function getAllowedTypesCustom(): array
+    {
+        return [];
+    }
+
+
+}
