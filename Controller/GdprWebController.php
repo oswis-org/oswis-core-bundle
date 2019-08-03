05f4544453b15af2066b647817b0d6153d7e7e9c
zakjakub <mail@jakubzak.eu>
2019-08-03 22:12:18 +0200

    Some changes.

---
+++ Controller/GdprWebController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Zakjakub\OswisCoreBundle\Controller;
+
+use LogicException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class GdprWebController extends AbstractController
+{
+
+    /**
+     * @return Response
+     * @throws LogicException
+     */
+    final public function gdprAction(): Response
+    {
+        return $this->render(
+            '@ZakjakubOswisCore/web/pages/gdpr.html.twig'
+        );
+    }
+
+}
