73a1f386b18141907e585c01fec10cc8f0e20a3e
zakjakub <mail@jakubzak.eu>
2019-08-25 16:19:53 +0200

    Reformat of code. Fixed return of revision from revision container.

--- Entity/AbstractClass/AbstractRevisionContainer.php
+++ Entity/AbstractClass/AbstractRevisionContainer.php
@@ -55,7 +55,6 @@ abstract class AbstractRevisionContainer implements RevisionContainerInterface
     abstract public static function checkRevision(?AbstractRevision $revision): void;
 
     /** @noinspection MethodShouldBeFinalInspection */
-
     /**
      * Remove some revision/version from this container.
      *
@@ -89,7 +88,6 @@ abstract class AbstractRevisionContainer implements RevisionContainerInterface
     }
 
     /** @noinspection MethodShouldBeFinalInspection */
-
     /**
      * Set revision/version which is actual/active now.
      *
@@ -125,15 +123,12 @@ abstract class AbstractRevisionContainer implements RevisionContainerInterface
      */
     final public function getRevision(DateTime $dateTime = null): AbstractRevision
     {
-        if (!$dateTime && $this->activeRevision) {
-            return $this->activeRevision;
-        }
         $revisions = $this->getRevisionsOlderThanDateTime($dateTime);
         if (!$revisions || !$revisions[0]) {
             throw new RevisionMissingException((static::getRevisionClassName() ?? 'Revision class').' not found.');
         }
         static::checkRevision($revisions[0]);
-        if (!$dateTime && !$this->activeRevision) {
+        if (!$dateTime && (!$this->activeRevision || $this->activeRevision !== $revisions[0])) {
             $this->activeRevision = $revisions[0];
         }
 
--- Form/AbstractClass/AbstractFileType.php
+++ Form/AbstractClass/AbstractFileType.php
@@ -51,7 +51,6 @@ abstract class AbstractFileType extends AbstractType
     abstract public static function getFileClassName(): string;
 
     /** @noinspection MethodShouldBeFinalInspection */
-
     public function getBlockPrefix(): string
     {
         return 'oswis_core_abstract_file';
