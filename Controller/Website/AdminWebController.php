aba2b5bda59da4709f48d1fe6d9096820f5efe4e
zakjakub <mail@jakubzak.eu>
2020-04-26 19:20:35 +0200

    Some fixes, updates and changes.

---
+++ Controller/Website/AdminWebController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @noinspection PhpUnused
+ */
+
+namespace OswisOrg\OswisCoreBundle\Controller\Website;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminWebController extends AbstractController
+{
+    /**
+     * Administration frontend placement wildcard route.
+     *
+     * @return Response
+     */
+    final public function admin(): Response
+    {
+        return $this->render('@OswisOrgOswisCore/web/pages/portal.html.twig');
+    }
+}
