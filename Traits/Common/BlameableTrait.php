a58bee34f94a8d663e2169ee5f7d6c86f121e7d7
zakjakub <mail@jakubzak.eu>
2020-05-08 00:28:41 +0200

    Some fixes, updates and changes.

--- Traits/Common/BlameableTrait.php
+++ Traits/Common/BlameableTrait.php
@@ -12,14 +12,14 @@ trait BlameableTrait
 {
     /**
      * @Gedmo\Mapping\Annotation\Blameable(on="create")
-     * @Doctrine\ORM\Mapping\ManyToOne(targetEntity="OswisOrg\OswisCoreBundle\Entity\AppUser")
+     * @Doctrine\ORM\Mapping\ManyToOne(targetEntity="OswisOrg\OswisCoreBundle\Entity\AppUser\AppUser")
      * @Doctrine\ORM\Mapping\JoinColumn(name="created_author_id", referencedColumnName="id")
      */
     protected ?AppUser $createdAuthor = null;
 
     /**
      * @Gedmo\Mapping\Annotation\Blameable(on="update")
-     * @Doctrine\ORM\Mapping\ManyToOne(targetEntity="OswisOrg\OswisCoreBundle\Entity\AppUser")
+     * @Doctrine\ORM\Mapping\ManyToOne(targetEntity="OswisOrg\OswisCoreBundle\Entity\AppUser\AppUser")
      * @Doctrine\ORM\Mapping\JoinColumn(name="updated_author_id", referencedColumnName="id")
      */
     protected ?AppUser $updatedAuthor = null;
