6d1d5f8c87fae240d797fbf0f1cdf44a543d166f
zakjakub <mail@jakubzak.eu>
2020-07-18 21:01:00 +0200

    Some fixes, updates and changes.

--- Controller/WebAdmin/WebSecurityController.php
+++ Controller/WebAdmin/WebSecurityController.php
@@ -5,10 +5,8 @@
 
 namespace OswisOrg\OswisCoreBundle\Controller\WebAdmin;
 
-use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class WebSecurityController extends AbstractController
@@ -24,12 +22,7 @@ class WebSecurityController extends AbstractController
         );
     }
 
-    /**
-     * @Route("/web_admin/logout", name="web_logout")
-     * @throws LogicException
-     */
-    final public function logout(): void
+    public function logout(): void
     {
-        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
