522eca699da502948d5f0cddbf2122047cc29f41
Jakub Zak <mail@jakubzak.eu>
2022-05-23 11:58:39 +0200

    Added redirection to portalUrl.

--- Controller/Web/PortalWebController.php
+++ Controller/Web/PortalWebController.php
@@ -4,18 +4,27 @@ declare(strict_types=1);
 
 namespace OswisOrg\OswisCoreBundle\Controller\Web;
 
+use OswisOrg\OswisCoreBundle\Provider\OswisCoreSettingsProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class PortalWebController extends AbstractController
 {
     /**
-     * Temporary Portal placement route.
+     * Temporary Portal placement route (or redirect if app.portalUrl is set).
+     *
+     * @param  \OswisOrg\OswisCoreBundle\Provider\OswisCoreSettingsProvider  $oswisCoreSettings
      *
      * @return Response
      */
-    final public function portal(): Response
-    {
+    final public function portal(
+        OswisCoreSettingsProvider $oswisCoreSettings,
+    ): Response {
+        $portalUrl = $oswisCoreSettings->getApp()['portalUrl'];
+        if (!empty($portalUrl)) {
+            return $this->redirect($portalUrl);
+        }
+
         return $this->render('@OswisOrgOswisCore/web/pages/portal.html.twig');
     }
 }
