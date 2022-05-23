6381cc97ef797659f2f5a03cdff96da1f34e28aa
Jakub Zak <mail@jakubzak.eu>
2022-05-23 11:45:10 +0200

    Updated dependencies and configuration.

--- Entity/Security/RefreshToken.php
+++ Entity/Security/RefreshToken.php
@@ -2,12 +2,12 @@
 
 namespace OswisOrg\OswisCoreBundle\Entity\Security;
 
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Table;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
-#[Entity()]
-#[Table(name: 'refresh_tokens')]
+/**
+ * @Doctrine\ORM\Mapping\Entity()
+ * @Doctrine\ORM\Mapping\Table(name="refresh_tokens")
+ */
 class RefreshToken extends BaseRefreshToken
 {
 
