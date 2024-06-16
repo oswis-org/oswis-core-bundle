6a11f5e3a4b981bebfc39fb70a9050398fa60bf6
Jakub Zak <mail@jakubzak.eu>
2024-06-16 10:59:48 +0200

    Updated dependencies.

--- Controller/WebAdmin/WebAdminController.php
+++ Controller/WebAdmin/WebAdminController.php
@@ -8,7 +8,7 @@ use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class WebAdminController extends AbstractController
 {
